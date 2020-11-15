600d24ec9d1a2c94e598a6b67bfc93308f0ba2a1
Ja Fajny <jendromij@gmail.com>
2020-11-15 13:15:38 +0100

    Now player can look at mouse cursor
    With great performance (no raycasting used)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,11 @@
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Transform body;
     [SerializeField] float movementSpeed;
     
     CharacterController characterController;
+    Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
 
     void Start()
     {
@@ -14,13 +16,29 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float hor = Input.GetAxisRaw("Horizontal");   
+        ControlKeyboardMouseInput();
+    }
+
+    void ControlKeyboardMouseInput()
+    {
+        //Movement
+        float hor = Input.GetAxisRaw("Horizontal");
         float ver = Input.GetAxisRaw("Vertical");
         MoveBy(new Vector3(hor, 0, ver).normalized);
+
+        //Look
+        RotateToScreenPoint(Input.mousePosition);
     }
 
     void MoveBy(Vector3 moveVecNorm)
     {
         characterController.Move(moveVecNorm * movementSpeed * Time.deltaTime);
+    }
+
+    void RotateToScreenPoint(Vector2 screenPoint)
+    {
+        Vector2 dirToCursor = (screenPoint - screenCenter).normalized;
+        Quaternion lookRot = Quaternion.LookRotation(new Vector3(dirToCursor.x, 0, dirToCursor.y));
+        body.rotation = lookRot;
     }
 }
