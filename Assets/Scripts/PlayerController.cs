ddd2d17bfc85d4c3d319018b81d3998f8de3a998
Ja Fajny <jendromij@gmail.com>
2020-11-15 12:37:41 +0100

    Added simple player controller with camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+    
+    CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");   
+        float ver = Input.GetAxisRaw("Vertical");
+        MoveBy(new Vector3(hor, 0, ver).normalized);
+    }
+
+    void MoveBy(Vector3 moveVecNorm)
+    {
+        characterController.Move(moveVecNorm * movementSpeed * Time.deltaTime);
+    }
+}
