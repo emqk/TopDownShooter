799b2e9358bd7fc8c2e2fae207841f01e22b5604
Ja Fajny <jendromij@gmail.com>
2020-11-16 21:27:09 +0100

    Added player rotation by swiping in MainMenu

---
+++ Assets/Scripts/MainMenu/PlayerRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerRotator : MonoBehaviour
+{
+    [SerializeField] float rotateSpeed;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 dragDistanceUnscaled = touch.deltaPosition;
+            float dragDistanceScaledX = dragDistanceUnscaled.x / Screen.width;
+            transform.Rotate(Vector3.up, -dragDistanceScaledX * rotateSpeed * Time.deltaTime);
+        }    
+    }
+}
