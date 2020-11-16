a8bb42030098d16cfb2e134d48e48639617c9d07
Ja Fajny <jendromij@gmail.com>
2020-11-16 14:20:50 +0100

    Added simple follow AI

---
+++ Assets/Scripts/AI/AI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
