00106f038bceee0c573a581b9886dabdfc0082ed
Emil Kuk <jendromij@gmail.com>
2021-03-16 18:17:14 +0100

    End panel - waves background depends on battle result

--- Assets/Scripts/UI/EndPanel/EndPanelUI.cs
+++ Assets/Scripts/UI/EndPanel/EndPanelUI.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class EndPanelUI : MonoBehaviour
 {
+    [SerializeField] Color wavesCompletedBackgroundColor;
+    [SerializeField] Color wavesNotCompletedBackgroundColor;
+    [SerializeField] Image wavesBackgroundImage;
     [SerializeField] TextMeshProUGUI wavesText;
     [SerializeField] TextMeshProUGUI moneyText;
 
@@ -12,13 +16,20 @@ public class EndPanelUI : MonoBehaviour
         int numOfWaves = SpawnManager.instance.GetNumberOfWaves();
         if (waveIndex + 1 >= numOfWaves)
         {
+            wavesBackgroundImage.color = wavesCompletedBackgroundColor;
             wavesText.text = "Map completed!";
         }
         else
         {
+            wavesBackgroundImage.color = wavesNotCompletedBackgroundColor;
             wavesText.text = waveIndex + " / " + numOfWaves;
         }
 
         moneyText.text = "Testing from code: 123";
+    }
+
+    void SetColorToWavesBackground(Color color) 
+    {
+        wavesBackgroundImage.color = color;
     }
 }
