3accaa073f9cdc2c73a503ccfc6c5a45362e899c
Emil Kuk <jendromij@gmail.com>
2021-03-18 20:01:06 +0100

    HorizontalView3D - Added rotation

--- Assets/Scripts/MainMenu/HorizontalView3D.cs
+++ Assets/Scripts/MainMenu/HorizontalView3D.cs
@@ -1,13 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HorizontalView3D : MonoBehaviour
 {
+    [Header("Settings")]
     [SerializeField] Transform contentParent;
     [SerializeField] float spacing;
 
+    [Header("Rotation")]
+    [SerializeField] bool rotate = true;
+    [SerializeField] float rotateSpeed = 10;
+    [SerializeField] float startRotation = 0;
+    float currentRotation = 0;
+
     List<GameObject> currentContentObjects = new List<GameObject>();
+
+
+    private void Update()
+    {
+        if (rotate)
+        {
+            RotateContent();
+        }
+    }
+
+    void RotateContent()
+    {
+        currentRotation += Time.deltaTime * rotateSpeed;
+        currentRotation %= 360;
+
+        foreach (GameObject current in currentContentObjects)
+        {
+            current.transform.localRotation = Quaternion.Euler(0, currentRotation, 0);
+        }
+    }
 
     public void SetContentObjects(GameObject[] contentObjects)
     {
@@ -29,5 +55,6 @@ public class HorizontalView3D : MonoBehaviour
         }
 
         currentContentObjects.Clear();
+        currentRotation = startRotation;
     }
 }
