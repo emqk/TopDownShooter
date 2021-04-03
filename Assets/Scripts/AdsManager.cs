7a5f60bf378295d5bd1abb1ffaef9c5ca7db9a6b
Emil Kuk <jendromij@gmail.com>
2021-04-03 17:10:45 +0200

    Added basic ad manager

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour, IUnityAdsListener
+{
+    const string rewardedPlacementId = "Rewarded_Android";
+    static string androidID = "4075311";
+    public static AdsManager instance;
+
+    void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            Initialize();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Initialize()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(androidID, true);
+    }
+
+    public void ShowSkippableAd()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+        else
+        {
+            Debug.Log("Skippable ad is not ready");
+        }
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady(rewardedPlacementId))
+        {
+            Advertisement.Show(rewardedPlacementId);
+        }
+        else
+        {
+            Debug.Log("Rewarded ad is not ready");
+        }
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        Debug.Log("Ad error: " + message);
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (showResult == ShowResult.Failed)
+        {
+            Debug.Log("Ad failed: " + placementId);
+        }
+        else if (showResult == ShowResult.Finished)
+        {
+            Debug.Log("Ad finished: " + placementId);
+            if (placementId == rewardedPlacementId)
+            {
+                MoneyManager.AddGold(10);
+                Debug.Log("Added gold from ad!");
+            }
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            Debug.Log("Ad skipped: " + placementId);
+        }
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        //Debug.Log("Ad start: " + placementId);
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        //Debug.Log("Ad ready: " + placementId);
+    }
+}
