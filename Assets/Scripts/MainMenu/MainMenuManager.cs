5264134d5e36ef59441130e1129a825c1e32ff44
Emil Kuk <jendromij@gmail.com>
2021-04-07 20:43:16 +0200

    Check if character/weapon/map data is valid before loading battle scene

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -21,16 +22,57 @@ public class MainMenuManager : MonoBehaviour
 
     public void Play()
     {
+        CharacterData characterData = Database.instance.GetCharacterData();
+        WeaponData weaponData = Database.instance.GetWeaponData();
         MapData mapData = Database.instance.GetMapData();
+
+        if (!characterData)
+        {
+            PopupData popupData = new PopupData()
+            {
+                title = "Can't start",
+                description = "Select a character before starting",
+                buttonsData = new List<PopupButttonData> { new PopupButttonData() { text = "Ok", onClick = PopupManager.instance.CloseLastPopup } },
+                popupType = PopupType.Bad
+            };
+            PopupManager.instance.CreatePopup(popupData);
+
+            Debug.Log("Can't load scene - characterData is null!");
+            return;
+        }
+        if (!weaponData)
+        {
+            PopupData popupData = new PopupData()
+            {
+                title = "Can't start",
+                description = "Select a weapon before starting",
+                buttonsData = new List<PopupButttonData> { new PopupButttonData() { text = "Ok", onClick = PopupManager.instance.CloseLastPopup } },
+                popupType = PopupType.Bad
+            };
+            PopupManager.instance.CreatePopup(popupData);
+
+            Debug.Log("Can't load scene - weaponData is null!");
+            return;
+        }
+        if (!mapData)
+        {
+            PopupData popupData = new PopupData() {
+                title = "Can't start"
+                , description = "Select a map before starting"
+                , buttonsData = new List<PopupButttonData> { new PopupButttonData() { text = "Ok", onClick = PopupManager.instance.CloseLastPopup } }
+                , popupType = PopupType.Bad };
+            PopupManager.instance.CreatePopup(popupData);
+
+            Debug.Log("Can't load scene - mapData is null!");
+            return;
+        }
+
+        //Is data is valid, load battle scene
         if (mapData)
         {
             Instantiate(loadingScreenPrefab);
             string sceneName = mapData.SceneName;
             StartCoroutine(DelayedLoadScene(sceneName));
-        }
-        else
-        {
-            Debug.LogError("Can't load scene - mapData is null!");
         }
     }
 
